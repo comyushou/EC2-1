711ffaf0183bad9b7d319ea4165dbd0ae29aafff
Motokawa <159shige@gmail.com>
2020-05-24 01:16:38 +0000

    [ADD]注文機能

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,10 @@ class Order < ApplicationRecord
   has_many :order_items
 
   enum is_payment_method: {クレジットカード:0, 銀行振り込み:1}
-
-
   enum order_status: {入金待ち:0,入金確認:1,製作中:2,発送準備中:3,発送済み:4}
   #上下必要分の為、付け足しました。
+
+#  validates :hoge, presence: true
 
 #orderのorder_itemを抜き出したい為に定義
 	def totalunit
@@ -17,5 +17,17 @@ class Order < ApplicationRecord
 	  end
 	  totalunit
 	end
+
+	def billing_amount
+		# この注文(selfのorder)に紐づく注文商品(order_item)全ての小計の合計 + 送料
+		self.order_items.each do |order_item|
+
+	end
+
+	def hoge
+		self.id
+	end
+
+end
 
 end
