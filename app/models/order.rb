37dcf76864079854306c0f4190c067dc2e7d0873
vagrant <vagrant@localhost.localdomain>
2020-05-25 07:25:54 +0000

    カラムの連動更新

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,11 @@ class Order < ApplicationRecord
   has_many :order_items
 
   enum is_payment_method: {クレジットカード:0, 銀行振り込み:1}
+
   enum order_status: {入金待ち:0,入金確認:1,製作中:2,発送準備中:3,発送済み:4}
   #上下必要分の為、付け足しました。
 
-  validates :user_id,:postage,:is_paymentmethod,:order_status,:review_name,:postal_code,:address,:billing_amount, presence: true
+  validates :user_id,:postage,:is_payment_method,:order_status,:review_name,:postal_code,:address,:billing_amount, presence: true
 
 #orderのorder_itemを抜き出したい為に定義
 	def totalunit
