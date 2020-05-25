f2e969a679eae95561c541f7ed83a3c52aefe895
Motokawa <159shige@gmail.com>
2020-05-25 12:37:14 +0000

    user論理削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_140351) do
+ActiveRecord::Schema.define(version: 2020_05_25_081634) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id", null: false
