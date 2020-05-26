60b3f393ddaa28f4d0506ed346abe56a6c83712b
vagrant <vagrant@localhost.localdomain>
2020-05-26 07:17:53 +0000

    ジャンル

--- app/controllers/users/home_controller.rb
+++ app/controllers/users/home_controller.rb
@@ -2,7 +2,10 @@ class Users::HomeController < ApplicationController
 	def top
 		@genres = Genre.all
 		batch_size = 4
-        @items = Item.offset(rand(Item.count-batch_size)).first(batch_size)
+		@items = Item.where(is_sale_status: true)  #販売ステータスが有効になっているものだけ表示
+        @itemitems = @items.offset(rand(@items.count-batch_size)).first(batch_size)
+
+
 	end
 	def about
 	end
